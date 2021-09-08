0ae21d6bbecb03fc7580c6b640c15c07bc2adf73
4molybdenum2 <tathagatapaul7@gmail.com>
2021-09-08 19:50:11 +0530

    added longest repeating subsequence problem

---
+++ gfg/dp/longest_repeating-subsequence.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+	public:
+		int LongestRepeatingSubsequence(string str){
+		    // Code here
+		    string str_ = str;
+		    int x = str.length();
+		    int y = str_.length();
+		    
+		    int dp[x+1][y+1];
+		    for(int i=0; i<= x; i++) dp[i][0] = 0;
+		    for(int j=0; j<= y; j++) dp[0][j] = 0;
+		    
+		    for(int i=1; i<=x; i++){
+		        for(int j=1; j<=x; j++){
+		            if(str[i-1] == str_[j-1] && i!=j)
+		                dp[i][j] = 1 + dp[i-1][j-1];
+		            else{
+		                dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+		            }
+		        }
+		    }
+		    
+		    return dp[x][y];
+		}
+
+};
